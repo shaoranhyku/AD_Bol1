9f374f4680d98fe622a8455242ad2946cc13d513
shaoranhyku <32264494+shaoranhyku@users.noreply.github.com>
2017-11-08 17:56:16 +0100

    Added Ejercicio14.java and json

--- Ejercicio13.xsl
+++ Ejercicio13.xsl
@@ -5,15 +5,25 @@
 			<xsl:apply-templates />
 		</html>
 	</xsl:template>
+
 	<xsl:template match="ListaContactos">
 		<head><title>LISTADO DE ALUMNOS</title></head>
 		<body>
 			<hl>LISTA DE ALUMNOS</hl>
 			<table border="l">
-				<tr><th>Nombre</th><th>Número de teléfono</th><th>Dirección</th><th>Código postal</th><th>Fecha de nacimiento</th><th>¿Deudas?</th><th>Cantidad</th></tr>
+				<tr>
+					<th>Nombre</th>
+					<th>Número de teléfono</th>
+					<th>Dirección</th>
+					<th>Código postal</th>
+					<th>Fecha de nacimiento</th>
+					<th>¿Deudas?</th>
+					<th>Cantidad</th>
+				</tr>
 				<xsl:apply-templates select="Contacto"/>
 			</table>
 		</body>
+
 	</xsl:template>
 	<xsl:template match="Contacto">
 		<tr><xsl:apply-templates /></tr>
---
+++ src/Ejercicio14.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+
+import java.io.*;
+
+public class Ejercicio14 {
+
+//     14 A partir del fichero XML de la agenda telefónica, obtener un fichero Json utilizando la
+//    librería Gson.
+
+    public static void main(String[] args) throws IOException {
+
+        Gson gson = new Gson();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Ejercicio14.json"));
+
+        Agenda agenda;
+
+        XStream xstream = new XStream();
+        xstream.alias("ListaContactos", Agenda.class);
+        xstream.alias("Contacto", PersonaAgenda.class);
+        xstream.addImplicitCollection(Agenda.class, "lista");
+
+        agenda= (Agenda) xstream.fromXML(new FileInputStream("Ejercicio10.xml"));
+
+        writer.write(gson.toJson(agenda));
+
+        writer.close();
+    }
+
+}
